9efbfdc51e87d2c93e3df8b431911c75a2dce2ef
AndreyKarabut <karabut.andrey2018@yandex.ru>
2022-08-25 20:02:03 +0600

    Initial commit

---
+++ src/main/java/com/example/tgbot/controllers/CurrencyController.java
@@ -0,0 +1,21 @@
+package com.example.tgbot.controllers;
+
+import com.example.tgbot.dto.ValuteCursOnDate;
+import com.example.tgbot.service.CentralRussianBankService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyController {
+
+    private final CentralRussianBankService centralRussianBankService;
+
+    @PostMapping("/getCurrencies")
+    public List<ValuteCursOnDate> getValuteCursOnDate() throws Exception {
+        return centralRussianBankService.getCurrenciesFromCbr();
+    }
+}
